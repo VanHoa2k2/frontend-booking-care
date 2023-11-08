a01667038986c3eea35bc94fce07a085b0fcdd9f
VanHoa2k2 <91736384+VanHoa2k2@users.noreply.github.com>
2023-11-09 02:04:56 +0700

    updates

--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -1,79 +1,102 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import Slider from "react-slick";
-import * as actions from "../../../store/actions";
-import {LANGUAGES} from "../../../utils";
 import { FormattedMessage } from "react-intl";
-import { withRouter } from 'react-router';
+import { withRouter } from "react-router";
 import { getAllHandbook } from "../../../services/userService";
 
 const HandBook = (props) => {
-  let {language} = props
-  console.log(props)
+  console.log(props);
   const [arrHandbooks, setArrHandbooks] = useState([]);
 
   useEffect(() => {
-    const fetchApiGetAllHandbook = async() => {
-      let res = await getAllHandbook()
+    const fetchApiGetAllHandbook = async () => {
+      let res = await getAllHandbook();
 
-      if(res && res.errCode === 0 ) {
-        setArrHandbooks(res.data ? res.data : [])
+      if (res && res.errCode === 0) {
+        setArrHandbooks(res.data ? res.data : []);
       }
-    }
-    fetchApiGetAllHandbook()
+    };
+    fetchApiGetAllHandbook();
   }, []);
 
   const handleViewDetailDoctor = (handbook) => {
-    props.history.push(`/detail-handbook/${handbook.id}`)
-  }
+    props.history.push(`/detail-handbook/${handbook.id}`);
+  };
 
-    return (
-      <div className="section-share section-handbook">
-        <div className="section-container pc">
-          <div className="section-header">
-            <span className="title-section">Cẩm nang</span>
-            <button className="btn-section">Xem thêm</button>
-          </div>
-          <div className="section-body">
-            <Slider {...props.settings}>
-            {arrHandbooks && arrHandbooks.length > 0 && arrHandbooks.map((item,i) => (
-            <div className="section-customize section-customize-handbook specialty-child" key={i}  onClick={() => handleViewDetailDoctor(item)}>
-              <div className="bg-img section-handbook" style={{ backgroundImage: `url(${item.image})`}} />
-              <div className="specialty-name">{item.name}</div>
-            </div>
-            ))}
-            </Slider>
-          </div>
+  return (
+    <div className="section-share section-handbook">
+      <div className="section-container pc">
+        <div className="section-header">
+          <span className="title-section">
+            {" "}
+            <FormattedMessage id="homepage.handbook" />
+          </span>
+          <button className="btn-section">
+            {" "}
+            <FormattedMessage id="homepage.more-info" />
+          </button>
         </div>
-
-        <div className="section-container mobile">
-          <div className="section-header">
-            <span className="title-section">Cẩm nang</span>
-            <button className="btn-section">Xem thêm</button>
-          </div>
-          <div className="section-body">
-            <Slider {...props.settingsMobile}>
-            {arrHandbooks && arrHandbooks.length > 0 && arrHandbooks.map((item,i) => (
-            <div className="section-customize section-customize-handbook specialty-child" key={i}  onClick={() => handleViewDetailDoctor(item)}>
-              <div className="bg-img section-handbook" style={{ backgroundImage: `url(${item.image})`}} />
-              <div className="specialty-name">{item.name}</div>
-            </div>
-            ))}
-            </Slider>
-          </div>
+        <div className="section-body">
+          <Slider {...props.settings}>
+            {arrHandbooks &&
+              arrHandbooks.length > 0 &&
+              arrHandbooks.map((item, i) => (
+                <div
+                  className="section-customize section-customize-handbook specialty-child"
+                  key={i}
+                  onClick={() => handleViewDetailDoctor(item)}
+                >
+                  <div
+                    className="bg-img section-handbook"
+                    style={{ backgroundImage: `url(${item.image})` }}
+                  />
+                  <div className="specialty-name">{item.name}</div>
+                </div>
+              ))}
+          </Slider>
         </div>
       </div>
-    );
-  };
+
+      <div className="section-container mobile">
+        <div className="section-header">
+          <span className="title-section">Cẩm nang</span>
+          <button className="btn-section">Xem thêm</button>
+        </div>
+        <div className="section-body">
+          <Slider {...props.settingsMobile}>
+            {arrHandbooks &&
+              arrHandbooks.length > 0 &&
+              arrHandbooks.map((item, i) => (
+                <div
+                  className="section-customize section-customize-handbook specialty-child"
+                  key={i}
+                  onClick={() => handleViewDetailDoctor(item)}
+                >
+                  <div
+                    className="bg-img section-handbook"
+                    style={{ backgroundImage: `url(${item.image})` }}
+                  />
+                  <div className="specialty-name">{item.name}</div>
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-    return {
-      isLoggedIn: state.user.isLoggedIn,
-    };
+  return {
+    isLoggedIn: state.user.isLoggedIn,
   };
-  
-  const mapDispatchToProps = (dispatch) => {
-    return {};
-  };
-  
-  export default withRouter(connect(mapStateToProps, mapDispatchToProps)(HandBook));
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(HandBook)
+);
--- src/containers/HomePage/Section/MedicalFacility.js
+++ src/containers/HomePage/Section/MedicalFacility.js
@@ -1,78 +1,105 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import "./MedicalFacility.scss"
+import "./MedicalFacility.scss";
 import Slider from "react-slick";
 import { getAllClinic } from "../../../services/userService";
 import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { FormattedMessage } from "react-intl";
 
 const MedicalFacility = (props) => {
-  const [dataClinics, setDataClinics] = useState([])
+  const [dataClinics, setDataClinics] = useState([]);
 
   useEffect(() => {
-    const callApiGetAllClinic = async() => {
-      let res = await getAllClinic()
-      if(res && res.data.length > 0) {
-        setDataClinics(res.data)
+    const callApiGetAllClinic = async () => {
+      let res = await getAllClinic();
+      if (res && res.data.length > 0) {
+        setDataClinics(res.data);
       }
-    }
-    callApiGetAllClinic()
-  },[])
+    };
+    callApiGetAllClinic();
+  }, []);
 
   const handleViewDetailClinics = (clinic) => {
-    if(props.history) {
-      props.history.push(`/detail-clinic/${clinic.id}`)
+    if (props.history) {
+      props.history.push(`/detail-clinic/${clinic.id}`);
     }
-  }
-  
-    return (
-      <div className="section-share section-medical-facility" id="clinic">
-        <div className="section-container pc">
-          <div className="section-header">
-            <span className="title-section">Cơ sở y tế nổi bật</span>
-            <button className="btn-section">Xem thêm</button>
-          </div>
-          <div className="section-body">
-            <Slider {...props.settings}>
-              {dataClinics && dataClinics.length > 0 && dataClinics.map((item, i) => (
-              <div className="section-customize clinic-child" key={i} onClick={() => handleViewDetailClinics(item)}>
-                <div className="bg-img section-medical-facility" style={{ backgroundImage: `url(${item.image})`}}  />
-                <div className="clinic-name">{item.name}</div>
-              </div>      
-              ))
-              }
-            </Slider>
-          </div>
-        </div>
+  };
 
-        <div className="section-container mobile">
-          <div className="section-header">
-            <span className="title-section">Cơ sở y tế nổi bật</span>
-            <button className="btn-section">Xem thêm</button>
-          </div>
-          <div className="section-body">
-            <Slider {...props.settingsMobile}>
-              {dataClinics && dataClinics.length > 0 && dataClinics.map((item, i) => (
-              <div className="section-customize clinic-child" key={i} onClick={() => handleViewDetailClinics(item)}>
-                <div className="bg-img section-medical-facility" style={{ backgroundImage: `url(${item.image})`}}  />
-                <div className="clinic-name">{item.name}</div>
-              </div>      
-              ))
-              }
-            </Slider>
-          </div>
+  return (
+    <div className="section-share section-medical-facility" id="clinic">
+      <div className="section-container pc">
+        <div className="section-header">
+          <span className="title-section">
+            <FormattedMessage id="homepage.outstanding-medical-facilities" />
+          </span>
+          <button className="btn-section">
+            <FormattedMessage id="homepage.more-info" />
+          </button>
+        </div>
+        <div className="section-body">
+          <Slider {...props.settings}>
+            {dataClinics &&
+              dataClinics.length > 0 &&
+              dataClinics.map((item, i) => (
+                <div
+                  className="section-customize clinic-child"
+                  key={i}
+                  onClick={() => handleViewDetailClinics(item)}
+                >
+                  <div
+                    className="bg-img section-medical-facility"
+                    style={{ backgroundImage: `url(${item.image})` }}
+                  />
+                  <div className="clinic-name">{item.name}</div>
+                </div>
+              ))}
+          </Slider>
         </div>
       </div>
-    );
-  };
+
+      <div className="section-container mobile">
+        <div className="section-header">
+          <span className="title-section">
+            <FormattedMessage id="homepage.specialty-popular" />
+          </span>
+          <button className="btn-section">
+            <FormattedMessage id="homepage.more-info" />
+          </button>
+        </div>
+        <div className="section-body">
+          <Slider {...props.settingsMobile}>
+            {dataClinics &&
+              dataClinics.length > 0 &&
+              dataClinics.map((item, i) => (
+                <div
+                  className="section-customize clinic-child"
+                  key={i}
+                  onClick={() => handleViewDetailClinics(item)}
+                >
+                  <div
+                    className="bg-img section-medical-facility"
+                    style={{ backgroundImage: `url(${item.image})` }}
+                  />
+                  <div className="clinic-name">{item.name}</div>
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-    return {
-      isLoggedIn: state.user.isLoggedIn,
-    };
+  return {
+    isLoggedIn: state.user.isLoggedIn,
   };
-  
-  const mapDispatchToProps = (dispatch) => {
-    return {};
-  };
-  
-  export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MedicalFacility));
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(MedicalFacility)
+);
